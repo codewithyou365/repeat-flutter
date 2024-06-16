07ea3cc2ec3579bf9adaea4755d39ebf05dd7b92
codewithyou365 <codewithyou365@gmail.com>
2024-06-16 23:00:17 +0800

    A SHA-1 hash algorithm has been added.

---
+++ lib/common/hash.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+import 'package:crypto/crypto.dart';
+import 'dart:async';
+
+class Hash {
+  static Future<String> toSha1(String filePath) async {
+    var file = File(filePath);
+
+    if (!await file.exists()) {
+      return "";
+    }
+
+    var inputStream = file.openRead();
+    final sha1Stream = sha1.bind(inputStream);
+
+    var sha1Hash = await sha1Stream.first;
+
+    return sha1Hash.bytes.map((byte) => byte.toRadixString(16).padLeft(2, '0')).join();
+  }
+}
