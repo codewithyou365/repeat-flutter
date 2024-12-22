21ea3e8937a4bef319c523d2768ab225cc05d1ba
codewithyou365 <codewithyou365@gmail.com>
2024-12-22 22:39:46 +0800

    Added a sample game to help remember.

--- game/repeat/bin/repeat.dart
+++ game/repeat/bin/repeat.dart
@@ -1,11 +1,84 @@
 import 'dart:convert';
-
+import 'dart:io';
+import 'package:repeat/common/string_util.dart';
 import 'package:repeat/common/ws/client.dart';
 import 'package:repeat/common/ws/message.dart';
 import 'package:repeat/common/ws/server.dart';
 import 'dart:isolate';
 
-void main() async {
+String replaceChar = '_';
+
+String processWord(String input, String display, String original) {
+  List<String> inputSegments = StringUtil.fields(input);
+  List<String> originalFields = StringUtil.fields(original);
+  List<String> displayFields = StringUtil.fields(display);
+  if (inputSegments.isEmpty) {
+    return display;
+  }
+  for (int i = 0; i < originalFields.length; i++) {
+    if (inputSegments.isNotEmpty) {
+      var rawDisplay = displayFields[i];
+      displayFields[i] = processChar(inputSegments[0], displayFields[i], originalFields[i]);
+      if (displayFields[i] != rawDisplay) {
+        inputSegments.removeAt(0);
+        if (inputSegments.isEmpty) {
+          break;
+        }
+      }
+    }
+  }
+
+  return displayFields.join(' ');
+}
+
+String processChar(String input, String currentDisplay, String original) {
+  return innerProcessChar(input.split(''), currentDisplay, original);
+}
+
+String innerProcessChar(List<String> inputChars, String currentDisplay, String original) {
+  List<String> originalChars = original.split('');
+  List<String> displayChars = currentDisplay.isEmpty ? List.filled(original.length, replaceChar) : currentDisplay.split('');
+
+  for (int i = 0; i < originalChars.length; i++) {
+    if (inputChars.isNotEmpty && originalChars[i].toLowerCase() == inputChars.first.toLowerCase()) {
+      displayChars[i] = originalChars[i];
+      inputChars.removeAt(0);
+    }
+  }
+
+  currentDisplay = displayChars.join('');
+  return currentDisplay;
+}
+
+void main() {
+  // Prompt the user for input
+  //String original = "This is an apple.";
+  String original = "A sample command-line application with an entrypoint in `bin/`, library code.";
+  String currentDisplay = "";
+  currentDisplay = processChar(original.replaceAll(RegExp(r'[\p{L}\p{N}]+', unicode: true), ''), currentDisplay, original);
+  print(currentDisplay);
+  var count = 100;
+  while (true) {
+    stdout.write('Enter: ');
+    String? input = stdin.readLineSync();
+    if (input == null || input.isEmpty) {
+      continue;
+    }
+    currentDisplay = processWord(input, currentDisplay, original);
+    print(currentDisplay);
+    count--;
+    if (currentDisplay == original) {
+      print("You win!");
+      break;
+    }
+    if (count == 0) {
+      print("You lose!");
+      break;
+    }
+  }
+}
+
+void main3() async {
   final receivePort = ReceivePort();
   Server s = Server();
   s.start(8089);
