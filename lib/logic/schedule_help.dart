4b4532cb894debf8c31aad874a77be35b689e428
codewithyou365 <codewithyou365@gmail.com>
2025-01-06 23:59:19 +0800

    Remove the aTemplate, as it confuses people.

--- lib/logic/schedule_help.dart
+++ lib/logic/schedule_help.dart
@@ -1,5 +1,4 @@
 import 'package:repeat_flutter/common/date.dart';
-import 'package:repeat_flutter/common/path.dart';
 import 'package:repeat_flutter/db/database.dart';
 import 'package:repeat_flutter/db/entity/classroom.dart';
 import 'package:repeat_flutter/db/entity/content.dart';
