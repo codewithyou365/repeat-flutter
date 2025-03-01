ce5d5f914e2540292fbf47f82a236b512edadf6b
codewithyou365 <codewithyou365@gmail.com>
2025-03-01 19:10:08 +0800

    Improved the snackbar widget.

--- lib/widget/snackbar/snackbar.dart
+++ lib/widget/snackbar/snackbar.dart
@@ -8,34 +8,30 @@ class Snackbar {
     if (context == null) {
       return;
     }
-    double marginWidth = 40;
-    if (content.length < 10) {
-      marginWidth = 80;
-    }
     var isDarkMode = Theme.of(context).brightness == Brightness.dark;
     ScaffoldMessenger.of(context).hideCurrentSnackBar();
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Center(
-          child: Text(
-            content,
-            style: TextStyle(
-              fontSize: 18,
-              color: isDarkMode ? Colors.white : Colors.black,
+        content: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: Text(
+                content,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: isDarkMode ? Colors.white : Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
             ),
-            textAlign: TextAlign.center,
           ),
         ),
-        duration: const Duration(seconds: 3),
+        duration: const Duration(seconds: 2),
         behavior: SnackBarBehavior.floating,
-        backgroundColor: isDarkMode ? Colors.black : Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        margin: EdgeInsets.only(
-          left: marginWidth,
-          right: marginWidth,
-          bottom: MediaQuery.of(context).size.height / 2 - 28,
-        ),
-        elevation: 10000000,
+        backgroundColor: Colors.transparent,
+        margin: const EdgeInsets.symmetric(horizontal: 16),
       ),
     );
   }
