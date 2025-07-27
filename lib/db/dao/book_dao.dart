109b7a092dbb493e3fe04098345357dafdf0563b
codewithyou365 <codewithyou365@gmail.com>
2025-07-27 19:56:02 +0800

    It needs to be reloaded after committing.

--- lib/db/dao/book_dao.dart
+++ lib/db/dao/book_dao.dart
@@ -11,7 +11,6 @@ import 'package:repeat_flutter/db/entity/classroom.dart';
 import 'package:repeat_flutter/db/entity/book.dart';
 import 'package:repeat_flutter/db/entity/content_version.dart';
 import 'package:repeat_flutter/db/entity/verse.dart';
-import 'package:repeat_flutter/db/entity/verse_content_version.dart';
 import 'package:repeat_flutter/i18n/i18n_key.dart';
 import 'package:repeat_flutter/logic/model/book_show.dart';
 import 'package:repeat_flutter/widget/snackbar/snackbar.dart';
