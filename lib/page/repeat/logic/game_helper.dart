728a67b966aa3f892905c201a7f01626dd7e6dbe
codewithyou365 <codewithyou365@gmail.com>
2026-01-23 11:34:17 +0800

    Do not insert the game table when refreshing the game, as the table will be deprecated.

--- lib/page/repeat/logic/game_helper.dart
+++ lib/page/repeat/logic/game_helper.dart
@@ -1,14 +1,10 @@
-import 'package:repeat_flutter/common/date.dart';
 import 'package:repeat_flutter/common/ws/message.dart';
 import 'package:repeat_flutter/db/database.dart';
-import 'package:repeat_flutter/db/entity/game.dart';
 import 'package:repeat_flutter/db/entity/kv.dart';
 import 'package:repeat_flutter/db/entity/verse_today_prg.dart';
 import 'package:repeat_flutter/logic/event_bus.dart';
 import 'package:repeat_flutter/logic/game_server/constant.dart';
 import 'package:repeat_flutter/logic/game_server/web_server.dart';
-import 'package:repeat_flutter/logic/model/verse_show.dart';
-import 'package:repeat_flutter/logic/verse_help.dart';
 
 class GameHelper {
   final WebServer server;
@@ -20,22 +16,7 @@ class GameHelper {
       return;
     }
     await Db().db.kvDao.insertKv(Kv(K.lastVerseId, '${stp.verseId}'));
-    VerseShow verse = VerseHelp.getCache(stp.verseId)!;
-    var now = DateTime.now();
     stp.time += 1;
-    var game = Game(
-      id: stp.id!,
-      time: stp.time,
-      verseContent: verse.verseContent,
-      verseId: verse.verseId,
-      classroomId: stp.classroomId,
-      bookId: verse.bookId,
-      chapterId: verse.chapterId,
-      finish: false,
-      createTime: now.millisecondsSinceEpoch,
-      createDate: Date.from(now),
-    );
-    await Db().db.gameDao.tryInsertGame(game);
     EventBus().publish(EventTopic.newGame, stp.verseId);
     await server.server.broadcast(
       Request(
